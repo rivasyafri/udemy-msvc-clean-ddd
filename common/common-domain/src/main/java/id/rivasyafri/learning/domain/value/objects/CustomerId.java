6f3fcfb60a0ab23ef3f0c9bbee851cab1494e048
Riva Syafri Rachmatullah <rivasyafri@gmail.com>
2024-08-17 11:45:47 +0700

    push implementation of service domain

--- common/common-domain/src/main/java/id/rivasyafri/learning/domain/value/objects/CustomerId.java
+++ common/common-domain/src/main/java/id/rivasyafri/learning/domain/value/objects/CustomerId.java
@@ -3,7 +3,7 @@ package id.rivasyafri.learning.domain.value.objects;
 import java.util.UUID;
 
 public class CustomerId extends BaseId<UUID> {
-  protected CustomerId(UUID value) {
+  public CustomerId(UUID value) {
     super(value);
   }
 }
