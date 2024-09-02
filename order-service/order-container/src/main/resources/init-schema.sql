f6c7178dd2e591e9257afde329465780421486d4
Riva Syafri Rachmatullah <rivasyafri@gmail.com>
2024-09-02 19:20:56 +0700

    feat: add customer service simple code for running for testing order
    This is needed to run the Order application

--- order-service/order-container/src/main/resources/init-schema.sql
+++ order-service/order-container/src/main/resources/init-schema.sql
@@ -1,15 +1,15 @@
-DROP SCHEMA IF EXISTS "order" CASCADE;
+DROP SCHEMA IF EXISTS order CASCADE;
 
-CREATE SCHEMA "order";
+CREATE SCHEMA order;
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DROP TYPE IF EXISTS order_status;
 CREATE TYPE order_status AS ENUM ('PENDING', 'PAID', 'APPROVED', 'CANCELLED', 'CANCELLING');
 
-DROP TABLE IF EXISTS "order".orders CASCADE;
+DROP TABLE IF EXISTS order.orders CASCADE;
 
-CREATE TABLE "order".orders
+CREATE TABLE order.orders
 (
     id uuid NOT NULL,
     customer_id uuid NOT NULL,
@@ -21,9 +21,9 @@ CREATE TABLE "order".orders
     CONSTRAINT orders_pkey PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS "order".order_items CASCADE;
+DROP TABLE IF EXISTS order.order_items CASCADE;
 
-CREATE TABLE "order".order_items
+CREATE TABLE order.order_items
 (
     id bigint NOT NULL,
     order_id uuid NOT NULL,
@@ -34,16 +34,16 @@ CREATE TABLE "order".order_items
     CONSTRAINT order_items_pkey PRIMARY KEY (id, order_id)
 );
 
-ALTER TABLE "order".order_items
+ALTER TABLE order.order_items
     ADD CONSTRAINT "FK_ORDER_ID" FOREIGN KEY (order_id)
-    REFERENCES "order".orders (id) MATCH SIMPLE
+    REFERENCES order.orders (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
 
-DROP TABLE IF EXISTS "order".order_address CASCADE;
+DROP TABLE IF EXISTS order.order_address CASCADE;
 
-CREATE TABLE "order".order_address
+CREATE TABLE order.order_address
 (
     id uuid NOT NULL,
     order_id uuid UNIQUE NOT NULL,
@@ -53,7 +53,7 @@ CREATE TABLE "order".order_address
     CONSTRAINT order_address_pkey PRIMARY KEY (id, order_id)
 );
 
-ALTER TABLE "order".order_address
+ALTER TABLE order.order_address
     ADD CONSTRAINT "FK_ORDER_ID" FOREIGN KEY (order_id)
     REFERENCES "order".orders (id) MATCH SIMPLE
     ON UPDATE NO ACTION
