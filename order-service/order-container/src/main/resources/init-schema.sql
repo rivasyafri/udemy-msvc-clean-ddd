a41c5d90660ef50725d532a0657d9ff6da5b57c6
Riva Syafri Rachmatullah <rivasyafri@gmail.com>
2024-10-19 22:16:44 +0700

    feat: adding schema for outbox for payment and restaurant approval

--- order-service/order-container/src/main/resources/init-schema.sql
+++ order-service/order-container/src/main/resources/init-schema.sql
@@ -1,15 +1,15 @@
-DROP SCHEMA IF EXISTS order CASCADE;
+DROP SCHEMA IF EXISTS "order" CASCADE;
 
-CREATE SCHEMA order;
+CREATE SCHEMA "order";
 
 -- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DROP TYPE IF EXISTS order_status;
 CREATE TYPE order_status AS ENUM ('PENDING', 'PAID', 'APPROVED', 'CANCELLED', 'CANCELLING');
 
-DROP TABLE IF EXISTS order.orders CASCADE;
+DROP TABLE IF EXISTS "order".orders CASCADE;
 
-CREATE TABLE order.orders
+CREATE TABLE "order".orders
 (
     id uuid NOT NULL,
     customer_id uuid NOT NULL,
@@ -21,9 +21,9 @@ CREATE TABLE order.orders
     CONSTRAINT orders_pkey PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS order.order_items CASCADE;
+DROP TABLE IF EXISTS "order".order_items CASCADE;
 
-CREATE TABLE order.order_items
+CREATE TABLE "order".order_items
 (
     id bigint NOT NULL,
     order_id uuid NOT NULL,
@@ -34,16 +34,16 @@ CREATE TABLE order.order_items
     CONSTRAINT order_items_pkey PRIMARY KEY (id, order_id)
 );
 
-ALTER TABLE order.order_items
+ALTER TABLE "order".order_items
     ADD CONSTRAINT "FK_ORDER_ID" FOREIGN KEY (order_id)
-    REFERENCES order.orders (id) MATCH SIMPLE
+    REFERENCES "order".orders (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
 
-DROP TABLE IF EXISTS order.order_address CASCADE;
+DROP TABLE IF EXISTS "order".order_address CASCADE;
 
-CREATE TABLE order.order_address
+CREATE TABLE "order".order_address
 (
     id uuid NOT NULL,
     order_id uuid UNIQUE NOT NULL,
@@ -53,9 +53,65 @@ CREATE TABLE order.order_address
     CONSTRAINT order_address_pkey PRIMARY KEY (id, order_id)
 );
 
-ALTER TABLE order.order_address
+ALTER TABLE "order".order_address
     ADD CONSTRAINT "FK_ORDER_ID" FOREIGN KEY (order_id)
     REFERENCES "order".orders (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
+
+DROP TYPE IF EXISTS saga_status;
+CREATE TYPE saga_status AS ENUM ('STARTED', 'FAILED', 'SUCCEEDED', 'PROCESSING', 'COMPENSATING', 'COMPENSATED');
+
+DROP TYPE IF EXISTS outbox_status;
+CREATE TYPE outbox_status AS ENUM ('STARTED', 'COMPLETED', 'FAILED');
+
+DROP TABLE IF EXISTS "order".payment_outbox CASCADE;
+
+CREATE TABLE "order".payment_outbox
+(
+    id uuid NOT NULL,
+    saga_id uuid NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE,
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    payload jsonb NOT NULL,
+    outbox_status outbox_status NOT NULL,
+    saga_status saga_status NOT NULL,
+    order_status order_status NOT NULL,
+    version integer NOT NULL,
+    CONSTRAINT payment_outbox_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX "payment_outbox_saga_status"
+    ON "order".payment_outbox
+    (type, outbox_status, saga_status);
+
+CREATE UNIQUE INDEX "payment_outbox_saga_id"
+    ON "order".payment_outbox
+    (type, saga_id, saga_status);
+
+DROP TABLE IF EXISTS "order".restaurant_approval_outbox CASCADE;
+
+CREATE TABLE "order".restaurant_approval_outbox
+(
+    id uuid NOT NULL,
+    saga_id uuid NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE,
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    payload jsonb NOT NULL,
+    outbox_status outbox_status NOT NULL,
+    saga_status saga_status NOT NULL,
+    order_status order_status NOT NULL,
+    version integer NOT NULL,
+    CONSTRAINT restaurant_approval_outbox_pkey PRIMARY KEY (id)
+)
+
+CREATE INDEX "restaurant_approval_outbox_saga_status"
+    ON "order".restaurant_approval_outbox
+        (type, outbox_status, saga_status);
+
+CREATE UNIQUE INDEX "restaurant_approval_outbox_saga_id"
+    ON "order".restaurant_approval_outbox
+        (type, saga_id, saga_status);
